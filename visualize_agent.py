9ac2d3fb007fdffdeb3f60a7b2d8b62a288aaa0e
unknown <raphael.montaud@gmail.com>
2019-01-14 17:34:40 +0100

    dunno

--- visualize_agent.py
+++ visualize_agent.py
@@ -8,11 +8,12 @@ np.random.seed(2)
 
 flow_type = "my_flow"
 features = ["count_incoming", "median_speed", "mean_speed"]
-agent = LinQAgent(mode="deep", features=features)
-agent.load("lin_q_pretrained")
-agent.set_is_online(True)
-agent.set_is_training(False)
+# agent = LinQAgent(mode="deep", features=features)
+# agent.load("lin_q_pretrained")
+# agent.set_is_online(True)
+# agent.set_is_training(False)
 
+agent = ConstantAgent(period=5)
 reward, n_switches, avg_travel_time = run_agent(
     agent, flow_type=flow_type, use_gui=True, max_t=5000, lane_type="uniform"
 )
