5219da092634a4913f5ddb71a71045d1c091f300
unknown <raphael.montaud@gmail.com>
2019-01-16 16:31:25 +0100

    agent class can be passed as argument

--- agents/multi_agent_wrapper.py
+++ agents/multi_agent_wrapper.py
@@ -7,12 +7,12 @@ from torch.utils.data import DataLoader
 
 from utils import get_phase, wgreen, get_state_sumo
 from agents.linqagent import LinQAgent
+from agents.fully_offline import OfflineAgent
 
 
 class MultiAgentWrapper:
 
-    def __init__(self, mode="lin", features=None, memory_palace=True):
-        self.mode = mode
+    def __init__(self, agent_class=LinQAgent, offsets=None, mode="deep", features=None, memory_palace=True):
         self.features = features
         self.memory_palace = memory_palace
 
@@ -23,13 +23,16 @@ class MultiAgentWrapper:
 
         # parameters
         self.agents = {}
-        i = 3
+
+        if offsets is None:
+            offsets = {node: i + 3 for (i, node) in enumerate(nodes)}
+
         for node in nodes:
-            self.agents[node] = LinQAgent(
-                mode=self.mode, features=self.features, node=node, memory_palace=self.memory_palace
+            self.agents[node] = agent_class(
+                mode=mode, features=self.features, node=node, memory_palace=self.memory_palace
             )
-            self.agents[node].offline_period = i
-            i += 1
+
+            self.agents[node].offline_period = offsets[node]
 
     def choose_action(self):
         ans = {}
